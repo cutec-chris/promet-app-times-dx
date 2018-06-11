f2cc653cb785a659f60e0f55a4dc522be175e6e4
Chris <ulrich@tcs-germany.de>
2018-06-11 14:19:44 +0200

    Replace by simple time app

---
+++ timereg.js
@@ -0,0 +1,7 @@
+﻿rtl.module("timereg",["System","Classes","Avamm"],function () {
+  "use strict";
+  var $mod = this;
+  $mod.$init = function () {
+    pas.System.Writeln("Hello World from Timereg...");
+  };
+});
---
+++ timereg.lpr
@@ -0,0 +1,7 @@
+library timereg;
+  uses js, web, classes, Avamm;
+
+initialization
+  writeln('Hello World from Timereg...');
+end.
+
