5c1645e1286746ff7cf9e3dd569ee6214e295134
Chris <ulrich@tcs-germany.de>
2018-06-11 15:21:21 +0200

    Register Route

--- timereg.js
+++ timereg.js
@@ -1,7 +1,12 @@
-﻿rtl.module("timereg",["System","Classes","Avamm"],function () {
+﻿rtl.module("timereg",["System","Classes","Avamm","webrouter"],function () {
   "use strict";
   var $mod = this;
+  this.ShowTimereg = function (URl, aRoute, Params) {
+    pas.System.Writeln("Timereg should be shown");
+  };
+  $mod.$resourcestrings = {strTimeregistering: {org: "Zeiterfassung"}};
   $mod.$init = function () {
     pas.System.Writeln("Hello World from Timereg...");
+    pas.Avamm.RegisterSidebarRoute(rtl.getResStr(pas.timereg,"strTimeregistering"),"timeregistering",$mod.ShowTimereg);
   };
 });
--- timereg.lpr
+++ timereg.lpr
@@ -1,7 +1,15 @@
 library timereg;
-  uses js, web, classes, Avamm;
+  uses js, web, classes, Avamm,webrouter;
 
+resourcestring
+  strTimeregistering     = 'Zeiterfassung';
+
+Procedure ShowTimereg(URl : String; aRoute : TRoute; Params: TStrings);
+begin
+  writeln('Timereg should be shown');
+end;
 initialization
   writeln('Hello World from Timereg...');
+  RegisterSidebarRoute(strTimeregistering,'timeregistering',@ShowTimereg);
 end.
 
