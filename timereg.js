308062ab6fa2601bcb2d29799a8dce7cb7f9cd41
Chris <ulrich@tcs-germany.de>
2018-06-13 15:53:59 +0200

    Initial Form

--- timereg.js
+++ timereg.js
@@ -1,8 +1,15 @@
-﻿rtl.module("timereg",["System","Classes","Avamm","webrouter"],function () {
+﻿rtl.module("timereg",["System","Web","Classes","Avamm","webrouter","AvammForms"],function () {
   "use strict";
   var $mod = this;
+  this.List = null;
   this.ShowTimereg = function (URl, aRoute, Params) {
+    var aParent = null;
     pas.System.Writeln("Timereg should be shown");
+    if (!($mod.List != null)) {
+      aParent = rtl.getObject(pas.Avamm.GetAvammContainer());
+      $mod.List = pas.AvammForms.TAvammListForm.$create("Create$1",[aParent,"times"]);
+    };
+    $mod.List.Show();
   };
   $mod.$resourcestrings = {strTimeregistering: {org: "Zeiterfassung"}};
   $mod.$init = function () {
@@ -10,3 +17,4 @@
     pas.Avamm.RegisterSidebarRoute(rtl.getResStr(pas.timereg,"strTimeregistering"),"timeregistering",$mod.ShowTimereg);
   };
 });
+//# sourceMappingURL=timereg.js.map
--- timereg.lpr
+++ timereg.lpr
@@ -1,12 +1,23 @@
 library timereg;
-  uses js, web, classes, Avamm,webrouter;
+  uses js, web, classes, Avamm,webrouter,AvammForms;
 
 resourcestring
   strTimeregistering     = 'Zeiterfassung';
 
+var
+  List: TAvammListForm = nil;
+
 Procedure ShowTimereg(URl : String; aRoute : TRoute; Params: TStrings);
+var
+  aParent: TJSHTMLElement;
 begin
   writeln('Timereg should be shown');
+  if not Assigned(List) then
+    begin
+      aParent := TJSHTMLElement(GetAvammContainer());
+      List := TAvammListForm.Create(aParent,'times');
+    end;
+  List.Show;
 end;
 initialization
   writeln('Hello World from Timereg...');
