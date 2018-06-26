847cb9682c321b3a2cfc2cb170e8db0d21226cd4
Chris <ulrich@tcs-germany.de>
2018-06-26 16:24:42 +0200

    Startet re-implementation

--- timereg.js
+++ timereg.js
@@ -1,20 +1,46 @@
-﻿rtl.module("timereg",["System","Web","Classes","Avamm","webrouter","AvammForms"],function () {
+﻿rtl.module("timereg",["System","JS","Web","Classes","Avamm","webrouter","AvammForms","SysUtils"],function () {
   "use strict";
   var $mod = this;
+  rtl.createClass($mod,"TTimeregForm",pas.AvammForms.TAvammListForm,function () {
+    this.ToolbarButtonClick = function (id) {
+      var aDate = null;
+      var tmp = "";
+      if (id === "refresh") {}
+      else if (id === "datep") {}
+      else if (id === "daten") {
+        tmp = "" + this.Toolbar.getValue("datea");
+        aDate = new Date(Date.parse(tmp));
+        aDate.setDate(aDate.getDate() - 1);
+        this.Toolbar.setValue("datea",pas.SysUtils.FormatDateTime("dd.MM.YYYY",pas.SysUtils.JSDateToDateTime(aDate)));
+        this.Refresh();
+      } else if (id === "new") ;
+    };
+    this.Refresh = function () {
+    };
+    this.Create$1 = function (aParent, aDataSet) {
+      pas.AvammForms.TAvammListForm.Create$1.call(this,aParent,aDataSet);
+      var $with1 = this.Toolbar;
+      $with1.addButton("new",0,rtl.getResStr(pas.timereg,"strNew"),"fa fa-plus-circle","fa fa-plus-circle");
+      $with1.addSeparator("sep1",1);
+      $with1.addButton("datep",2,"","fa fa-chevron-left","fa fa-chevron-left");
+      $with1.addInput("datea",3,"",null);
+      $with1.addButton("daten",4,"","fa fa-chevron-right","fa fa-chevron-right");
+      $with1.addSeparator("sep2",1);
+      $with1.attachEvent("onClick",rtl.createCallback(this,"ToolbarButtonClick"));
+    };
+  });
   this.List = null;
   this.ShowTimereg = function (URl, aRoute, Params) {
     var aParent = null;
-    pas.System.Writeln("Timereg should be shown");
     if (!($mod.List != null)) {
       aParent = rtl.getObject(pas.Avamm.GetAvammContainer());
-      $mod.List = pas.AvammForms.TAvammListForm.$create("Create$1",[aParent,"times"]);
+      $mod.List = $mod.TTimeregForm.$create("Create$1",[aParent,"times"]);
     };
     $mod.List.Show();
   };
-  $mod.$resourcestrings = {strTimeregistering: {org: "Zeiterfassung"}};
+  $mod.$resourcestrings = {strTimeregistering: {org: "Zeiterfassung"}, strNew: {org: "Neu"}};
   $mod.$init = function () {
-    pas.System.Writeln("Hello World from Timereg...");
-    pas.Avamm.RegisterSidebarRoute(rtl.getResStr(pas.timereg,"strTimeregistering"),"timeregistering",$mod.ShowTimereg);
+    if (pas.Avamm.getRight("timereg") > 0) pas.Avamm.RegisterSidebarRoute(rtl.getResStr(pas.timereg,"strTimeregistering"),"timeregistering",$mod.ShowTimereg);
   };
 });
 //# sourceMappingURL=timereg.js.map
--- timereg.lpr
+++ timereg.lpr
@@ -1,8 +1,21 @@
 library timereg;
-  uses js, web, classes, Avamm,webrouter,AvammForms;
+  uses js, web, classes, Avamm, webrouter, AvammForms, SysUtils;
+
+type
+
+  { TTimeregForm }
+
+  TTimeregForm = class(TAvammListForm)
+  protected
+    procedure ToolbarButtonClick(id : string);
+    procedure Refresh;
+  public
+    constructor Create(aParent: TJSElement; aDataSet: string);override;
+  end;
 
 resourcestring
   strTimeregistering     = 'Zeiterfassung';
+  strNew                 = 'Neu';
 
 var
   List: TAvammListForm = nil;
@@ -11,16 +24,63 @@ Procedure ShowTimereg(URl : String; aRoute : TRoute; Params: TStrings);
 var
   aParent: TJSHTMLElement;
 begin
-  writeln('Timereg should be shown');
   if not Assigned(List) then
     begin
       aParent := TJSHTMLElement(GetAvammContainer());
-      List := TAvammListForm.Create(aParent,'times');
+      List := TTimeregForm.Create(aParent,'times');
     end;
   List.Show;
 end;
+
+{ TTimeregForm }
+
+procedure TTimeregForm.ToolbarButtonClick(id : string);
+var
+  aDate: TJSDate;
+  tmp: String;
+begin
+  if (id='refresh') then
+    begin
+    end
+  else if (id='datep') then
+    begin
+    end
+  else if (id='daten') then
+    begin
+      tmp := string(Toolbar.getValue('datea'));
+      aDate := TJSDate.New(TJSDate.parse(tmp));
+      aDate.Date := aDate.Date - 1;
+      Toolbar.setValue('datea', FormatDateTime('dd.MM.YYYY',JSDateToDateTime(aDate)));
+      Refresh;
+    end
+  else if (id='new') then
+    begin
+    end
+  ;
+end;
+
+procedure TTimeregForm.Refresh;
+begin
+
+end;
+
+constructor TTimeregForm.Create(aParent: TJSElement; aDataSet: string);
+begin
+  inherited Create(aParent, aDataSet);
+  with Toolbar do
+    begin
+      addButton('new',0,strNew,'fa fa-plus-circle','fa fa-plus-circle');
+      addSeparator('sep1',1);
+      addButton('datep',2,'','fa fa-chevron-left','fa fa-chevron-left');
+      addInput('datea',3,'',null);
+      addButton('daten',4,'','fa fa-chevron-right','fa fa-chevron-right');
+      addSeparator('sep2',1);
+      attachEvent('onClick', @ToolbarButtonClick);
+    end;
+end;
+
 initialization
-  writeln('Hello World from Timereg...');
-  RegisterSidebarRoute(strTimeregistering,'timeregistering',@ShowTimereg);
+  if getRight('timereg')>0 then
+    RegisterSidebarRoute(strTimeregistering,'timeregistering',@ShowTimereg);
 end.
 
